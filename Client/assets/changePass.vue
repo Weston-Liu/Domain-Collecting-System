353fa8140e422bc14937536f0fee67512c6e6e7d
Weston-Liu <vkki@qq.com>
2016-12-29 18:00:59 +0800

    add change pass form validate

--- Client/assets/changePass.vue
+++ Client/assets/changePass.vue
@@ -1,14 +1,14 @@
 <template>
     <el-dialog title="Change Password" v-model="visible" :show-close="false" :close-on-click-modal="false">
-        <el-form>
-            <el-form-item label="Original Password">
-                <el-input v-model="op" auto-complete="off"></el-input>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+            <el-form-item label="Original Password" prop="op">
+                <el-input v-model="ruleForm.op" auto-complete="off"></el-input>
             </el-form-item>
-            <el-form-item label="New Password">
-                <el-input v-model="np" auto-complete="off"></el-input>
+            <el-form-item label="New Password" prop="np">
+                <el-input type="password" v-model="ruleForm.np" auto-complete="off"></el-input>
             </el-form-item>
-            <el-form-item label="Confirm New Password">
-                <el-input v-model="cnp" auto-complete="off"></el-input>
+            <el-form-item label="Confirm New Password" prop="cnp">
+                <el-input type="password" v-model="ruleForm.cnp" auto-complete="off"></el-input>
             </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -21,15 +21,67 @@
     export default {
         props: ['visible'],
         data() {
+            var validateOldPass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('Please input the password.'));
+                } else {
+                    callback();
+                }
+            };
+            var validateNewPass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('Please input the password.'));
+                } else {
+                    if (this.ruleForm.np !== '') {
+                        this.$refs.ruleForm.validateField('cnp');
+                    }
+                    callback();
+                }
+            };
+            var validateNewPassConfirm = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('Please input the new password again.'));
+                } else if (value !== this.ruleForm.np) {
+                    callback(new Error('Two new passwords are not the same.'));
+                } else {
+                    callback();
+                }
+            };
             return {
-                op: '',
-                np: '',
-                cnp: ''
+                ruleForm: {
+                    op: '',
+                    np: '',
+                    cnp: ''
+                },
+                rules: {
+                    op: [{
+                        validator: validateOldPass,
+                        trigger: 'change'
+                    }],
+                    np: [{
+                        validator: validateNewPass,
+                        trigger: 'blur'
+                    }],
+                    cnp: [{
+                        validator: validateNewPassConfirm,
+                        trigger: 'change'
+                    }]
+                }
             };
         },
         methods: {
             close: function () {
                 this.$emit('cpclosed');
+            },
+            submit: function (ev) {
+                this.$refs.ruleForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
             }
         }
     };
